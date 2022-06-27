3c90e287eed8fe418b05cfa78e818a22e014f76f
xxxx <xxxx>
2022-06-27 16:35:46 +0800

    Description: Add Audio Mic Client Code
    Match-id-7a85e02748a2bdb19149541935403acd7486644f

---
+++ services/audioclient/micclient/include/dmic_client.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_DMIC_CLIENT_H
+#define OHOS_DMIC_CLIENT_H
+
+#include <condition_variable>
+#include <cstdint>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <thread>
+
+#include "audio_capturer.h"
+#include "audio_info.h"
+
+#include "audio_data.h"
+#include "audio_encode_transport.h"
+#include "audio_event.h"
+#include "audio_param.h"
+#include "daudio_errorcode.h"
+#include "daudio_log.h"
+#include "iaudio_datatrans_callback.h"
+#include "iaudio_event_callback.h"
+
+namespace OHOS {
+namespace DistributedHardware {
+class DMicClient : public IAudioDataTransCallback, public std::enable_shared_from_this<DMicClient> {
+public:
+    DMicClient(const std::string &devId, const std::shared_ptr<IAudioEventCallback> &callback)
+        : devId_(devId), eventCallback_(callback) {};
+    ~DMicClient();
+    int32_t OnStateChange(int32_t type) override;
+    int32_t SetUp(const AudioParam &param);
+    int32_t StartCapture();
+    int32_t StopCapture();
+
+private:
+    void CaptureThreadRunning();
+
+private:
+    constexpr static const char *LOG_TAG = "DMicClient";
+    constexpr static size_t NUMBER_ZERO = 0;
+    constexpr static uint8_t CHANNEL_WAIT_SECONDS = 5;
+
+    std::string devId_;
+    std::mutex channelWaitMutex_;
+    std::thread captureDataThread_;
+    std::condition_variable channelWaitCond_;
+    AudioParam audioParam_;
+    bool isBlocking = false;
+    bool isCaptureReady = false;
+    bool isChannelReady_ = false;
+
+    std::shared_ptr<IAudioEventCallback> eventCallback_;
+    std::unique_ptr<AudioStandard::AudioCapturer> audioCapturer_;
+    std::shared_ptr<AudioEncodeTransport> micTrans_ = nullptr;
+};
+} // DistributedHardware
+} // OHOS
+#endif // OHOS_DMIC_CLIENT_H
---
+++ services/audioclient/micclient/src/dmic_client.cpp
@@ -0,0 +1,195 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "dmic_client.h"
+
+#include <chrono>
+
+namespace OHOS {
+namespace DistributedHardware {
+DMicClient::~DMicClient()
+{
+    if (micTrans_ != nullptr) {
+        DHLOGI("%s: ~DMicClient. Release mic client.", LOG_TAG);
+        StopCapture();
+    }
+}
+
+int32_t DMicClient::OnStateChange(int32_t type)
+{
+    DHLOGI("%s: On state change type: %d.", LOG_TAG, type);
+    switch (type) {
+        case AudioEventType::DATA_OPENED: {
+            isChannelReady_ = true;
+            std::unique_lock<std::mutex> lck(channelWaitMutex_);
+            channelWaitCond_.notify_all();
+            DHLOGI("%s: Data opened.", LOG_TAG);
+            isBlocking = true;
+            isCaptureReady = true;
+            captureDataThread_ = std::thread(&DMicClient::CaptureThreadRunning, this);
+            return DH_SUCCESS;
+        }
+        case AudioEventType::DATA_CLOSED: {
+            DHLOGI("%s: Data closed.", LOG_TAG);
+            return DH_SUCCESS;
+        }
+        default:
+            DHLOGE("%s: Invalid parameter type: %d.", LOG_TAG, type);
+            return ERR_DH_AUDIO_CLIENT_STATE_IS_INVALID;
+    }
+    return DH_SUCCESS;
+}
+
+int32_t DMicClient::SetUp(const AudioParam &param)
+{
+    DHLOGI("%s: Set mic client parameters {sampleRate: %d, bitFormat: %d, channelMask: %d, sourceType: %d}.", LOG_TAG,
+        audioParam_.comParam.sampleRate, audioParam_.comParam.bitFormat, audioParam_.comParam.channelMask,
+        audioParam_.CaptureOpts.sourceType);
+    audioParam_ = param;
+    AudioStandard::AudioCapturerOptions capturerOptions = {
+        {
+            AudioStreamInfo : static_cast<AudioStandard::AudioSamplingRate>(audioParam_.comParam.sampleRate),
+            AudioStandard::AudioEncodingType::ENCODING_PCM,
+            static_cast<AudioStandard::AudioSampleFormat>(audioParam_.comParam.bitFormat),
+            static_cast<AudioStandard::AudioChannel>(audioParam_.comParam.channelMask)
+        },
+        { AudioCapturerInfo : static_cast<AudioStandard::SourceType>(audioParam_.CaptureOpts.sourceType), NUMBER_ZERO }
+    };
+    audioCapturer_ = AudioStandard::AudioCapturer::Create(capturerOptions);
+    if (audioCapturer_ == nullptr) {
+        DHLOGE("%s: Audio capturer create failed.", LOG_TAG);
+        return ERR_DH_AUDIO_CLIENT_CREATE_CAPTURER_FAILED;
+    }
+
+    if (micTrans_ == nullptr) {
+        micTrans_ = std::make_shared<AudioEncodeTransport>(devId_);
+    }
+    int32_t ret = micTrans_->SetUp(audioParam_, audioParam_, shared_from_this());
+    if (ret != DH_SUCCESS) {
+        DHLOGE("%s: Mic trans setup failed.", LOG_TAG);
+        return ret;
+    }
+    return DH_SUCCESS;
+}
+
+int32_t DMicClient::StartCapture()
+{
+    DHLOGI("%s: Start capturer.", LOG_TAG);
+    if (audioCapturer_ == nullptr || micTrans_ == nullptr) {
+        DHLOGE("%s: The capturer or mictrans is not instantiated.", LOG_TAG);
+        return ERR_DH_AUDIO_CLIENT_CAPTURER_OR_MICTRANS_INSTANCE;
+    }
+    if (!audioCapturer_->Start()) {
+        DHLOGE("%s: Audio capturer start failed.", LOG_TAG);
+        if (!audioCapturer_->Release()) {
+            DHLOGE("%s: Audio capturer release failed.", LOG_TAG);
+            return ERR_DH_AUDIO_CLIENT_CAPTURER_RELEASE_FAILED;
+        }
+        return ERR_DH_AUDIO_CLIENT_CAPTURER_START_FAILED;
+    }
+
+    int32_t ret = micTrans_->Start();
+    if (ret != DH_SUCCESS) {
+        DHLOGE("%s: Mic trans start failed.", LOG_TAG);
+        return ret;
+    }
+    DHLOGI("%s: Wait for channel session opened.", LOG_TAG);
+    std::unique_lock<std::mutex> lck(channelWaitMutex_);
+    auto status = channelWaitCond_.wait_for(lck, std::chrono::seconds(CHANNEL_WAIT_SECONDS),
+        [this]() { return isChannelReady_; });
+    if (!status) {
+        DHLOGI("%s: Open channel session timeout(%ds).", LOG_TAG, CHANNEL_WAIT_SECONDS);
+        return ERR_DH_AUDIO_CLIENT_TRANS_TIMEOUT;
+    }
+    return DH_SUCCESS;
+}
+
+void DMicClient::CaptureThreadRunning()
+{
+    DHLOGI("%s: Start the capturer thread.", LOG_TAG);
+    size_t bufferLen = NUMBER_ZERO;
+    int32_t ret = audioCapturer_->GetBufferSize(bufferLen);
+    if (ret != DH_SUCCESS) {
+        DHLOGE("%s: Failed to get minimum buffer.", LOG_TAG);
+        return;
+    }
+    DHLOGI("%s: Obtains the minimum buffer length, bufferlen: %d.", LOG_TAG, bufferLen);
+    while (isCaptureReady) {
+        std::shared_ptr<AudioData> audioData = std::make_shared<AudioData>(bufferLen);
+        size_t bytesRead = NUMBER_ZERO;
+        while (bytesRead < bufferLen) {
+            auto start = std::chrono::high_resolution_clock::now();
+            int32_t len = audioCapturer_->Read(*(audioData->Data() + bytesRead), bufferLen - bytesRead, isBlocking);
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+            DHLOGI("%s: Audio Capturer Read in microseconds TimeTaken =(%ds).", LOG_TAG, (long long)duration.count());
+            if (len >= NUMBER_ZERO) {
+                bytesRead += len;
+            } else {
+                bytesRead = len;
+                break;
+            }
+        }
+        if (bytesRead < NUMBER_ZERO) {
+            DHLOGI("%s: Bytes read failed, bytesRead: %d.", LOG_TAG, bytesRead);
+            break;
+        } else if (bytesRead == NUMBER_ZERO) {
+            continue;
+        }
+
+        int32_t ret = micTrans_->FeedAudioData(audioData);
+        if (ret != DH_SUCCESS) {
+            DHLOGE("%s: Failed to send data.", LOG_TAG);
+        }
+    }
+}
+
+int32_t DMicClient::StopCapture()
+{
+    DHLOGI("%s: Stop capturer.", LOG_TAG);
+    if (audioCapturer_ == nullptr || micTrans_ == nullptr) {
+        DHLOGE("%s: The capturer or mictrans is not instantiated.", LOG_TAG);
+        return ERR_DH_AUDIO_CLIENT_CAPTURER_OR_MICTRANS_INSTANCE;
+    }
+    isBlocking = false;
+    isCaptureReady = false;
+    if (captureDataThread_.joinable()) {
+        captureDataThread_.join();
+    }
+
+    int32_t ret = micTrans_->Stop();
+    if (ret != DH_SUCCESS) {
+        DHLOGE("%s: Mic trans stop failed.", LOG_TAG);
+        return ret;
+    }
+    ret = micTrans_->Release();
+    if (ret != DH_SUCCESS) {
+        DHLOGE("%s: Mic trans release failed.", LOG_TAG);
+        return ret;
+    }
+    micTrans_ = nullptr;
+
+    if (!audioCapturer_->Stop()) {
+        DHLOGE("%s: Audio capturer stop failed.", LOG_TAG);
+        return ERR_DH_AUDIO_CLIENT_CAPTURER_STOP_FAILED;
+    }
+    if (!audioCapturer_->Release()) {
+        DHLOGE("%s: Audio capturer release failed.", LOG_TAG);
+        return ERR_DH_AUDIO_CLIENT_CAPTURER_FREE_FAILED;
+    }
+    return DH_SUCCESS;
+}
+} // DistributedHardware
+} // OHOS
