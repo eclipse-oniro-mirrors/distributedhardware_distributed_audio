0641347eaeedd783b1f4e75b86c522ba65d98266
xxxx <xxxx>
2022-07-04 16:54:31 +0800

    Description: add distributed_audio processor project
    Match-id-2286c5cf4eab2bebe8bd4a3105895cabd2e9579b

---
+++ services/audioprocessor/test/unittest/common/audio_processor_callback_test.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "audio_data.h"
+#include "audio_event.h"
+#include "daudio_log.h"
+#include "iaudio_processor_callback.h"
+
+namespace OHOS {
+namespace DistributedHardware {
+class AudioProcessorCallbackTest : public IAudioProcessorCallback {
+public:
+    AudioProcessorCallbackTest() {};
+    ~AudioProcessorCallbackTest() = default;
+
+    void OnAudioDataDone(const std::shared_ptr<AudioData> &outputData) override;
+    void OnStateNotify(const AudioEvent &event) override;
+
+private:
+    int32_t outputCnt_ = 0;
+};
+
+void AudioProcessorCallbackTest::OnAudioDataDone(const std::shared_ptr<AudioData> &outputData)
+{
+    DHLOGD("Test : OnAudioDataDone.");
+    outputCnt_++;
+    DHLOGD("Test : output the [%d]th processed audiodata, size %d sucessfuly.", outputCnt_, outputData->Size());
+}
+
+void AudioProcessorCallbackTest::OnStateNotify(const AudioEvent &event)
+{
+    DHLOGE("Test : OnStateNotify, event: %d.", event.type);
+}
+} // namespace DistributedHardware
+} // namespace OHOS
