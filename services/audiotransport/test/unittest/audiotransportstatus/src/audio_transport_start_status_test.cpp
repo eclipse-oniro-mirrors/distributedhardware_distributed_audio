71180a7c139417331756883e9ffdeb88f3ef85c7
xxxx <xxxx>
2022-10-27 16:10:34 +0800

    Description: State Mode ut
    Match-id-20ac16f497e6cd1d9c9b76ab55224861e42336be

---
+++ services/audiotransport/test/unittest/audiotransportstatus/src/audio_transport_start_status_test.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "audio_transport_start_status_test.h"
+
+#include "audio_transport_context.h"
+#include "daudio_errorcode.h"
+#include "daudio_log.h"
+#include "daudio_util.h"
+#include "mock_audio_data_channel.h"
+#include "mock_audio_processor.h"
+#include "securec.h"
+
+using namespace testing::ext;
+
+namespace OHOS {
+namespace DistributedHardware {
+
+void AudioTransportStartStatusTest::SetUpTestCase(void)
+{
+}
+
+void AudioTransportStartStatusTest::TearDownTestCase(void)
+{
+}
+
+void AudioTransportStartStatusTest::SetUp(void)
+{
+    audioChannel_ = std::shared_ptr<MockAudioDataChannel>();
+    processor_ = std::shared_ptr<MockIAudioProcessor>();
+    stateContext_ = std::shared_ptr<AudioTransportContext>();
+    audioStatus_ = std::make_shared<AudioTransportStartStatus>(stateContext_);
+}
+
+void AudioTransportStartStatusTest::TearDown(void)
+{
+    audioChannel_ = nullptr;
+    processor_ = nullptr;
+    stateContext_ = nullptr;
+    audioStatus_ = nullptr;
+}
+
+/**
+ * @tc.name: transport_start_test_001
+ * @tc.desc: Verify start action when status is start.
+ * @tc.type: FUNC
+ * @tc.require: AR000H0E5U
+ */
+HWTEST_F(AudioTransportStartStatusTest, transport_start_test_001, TestSize.Level1)
+{
+    EXPECT_EQ(DH_SUCCESS, audioStatus_->Start(audioChannel_, processor_));
+}
+
+/**
+ * @tc.name: transport_start_test_002
+ * @tc.desc: Verify stop action when status is start.
+ * @tc.type: FUNC
+ * @tc.require: AR000H0E5U
+ */
+HWTEST_F(AudioTransportStartStatusTest, transport_start_test_002, TestSize.Level1)
+{
+    EXPECT_EQ(ERR_DH_AUDIO_NULLPTR, audioStatus_->Stop(audioChannel_, processor_));
+}
+
+/**
+ * @tc.name: transport_start_test_003
+ * @tc.desc: Verify pause action when status is start.
+ * @tc.type: FUNC
+ * @tc.require: AR000H0E5U
+ */
+HWTEST_F(AudioTransportStartStatusTest, transport_start_test_003, TestSize.Level1)
+{
+    EXPECT_EQ(ERR_DH_AUDIO_NULLPTR, audioStatus_->Pause(processor_));
+}
+
+/**
+ * @tc.name: transport_start_test_004
+ * @tc.desc: Verify action when status is start.
+ * @tc.type: FUNC
+ * @tc.require: AR000H0E5U
+ */
+HWTEST_F(AudioTransportStartStatusTest, transport_start_test_004, TestSize.Level1)
+{
+    AudioParam testLocalParaEnc = {
+        {
+            SAMPLE_RATE_48000,
+            STEREO,
+            SAMPLE_S16LE,
+            AUDIO_CODEC_FLAC
+        },
+        {
+            SOURCE_TYPE_INVALID,
+            0
+        },
+        {
+            CONTENT_TYPE_UNKNOWN,
+            STREAM_USAGE_UNKNOWN,
+            0
+        }
+    };
+    AudioParam testRemoteParaEnc = {
+        {
+            SAMPLE_RATE_48000,
+            STEREO,
+            SAMPLE_S16LE,
+            AUDIO_CODEC_FLAC
+        },
+        {
+            SOURCE_TYPE_INVALID,
+            0
+        },
+        {
+            CONTENT_TYPE_UNKNOWN,
+            STREAM_USAGE_UNKNOWN,
+            0
+        }
+    };
+    EXPECT_EQ(DH_SUCCESS, audioStatus_->Restart(testLocalParaEnc, testRemoteParaEnc, processor_));
+}
+} // namespace DistributedHardware
+} // namespace OHOS
